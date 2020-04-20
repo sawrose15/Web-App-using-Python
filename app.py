d8026f2aa5b49424747b4726b6b1320c36aca94a
Saroj Maharjan <saroj_maharjan1993@yahoo.com>
2020-04-20 13:01:39 +0800

    Conditional Functions

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+# from flask import Flask,render_template
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#     headline = "Hello World!"
+#     return render_template("index.html", headline=headline)
+
+# @app.route('/bye')
+# def bye():
+#     headline = "Goodbye!"
+#     return render_template("index.html", headline=headline)
+
+
+# if __name__ == '__main__':
+#     app.run(debug= True)
+
+
+import datetime
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    app.run()
+def index():
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    new_year = True
+    return render_template('new_year_index.html',new_year = new_year)
